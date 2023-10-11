979ab0fca00ba1ce7d8e79fe3c9e6a01d25c87a2
Vinicius Silva <viniciuslucio792@gmail.com>
2023-10-11 13:28:03 -0300

    importei o menu superior

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,15 @@ import Cadastro from "./paginas/cadastro";
 import Home from "./paginas/home";
 import Rodape from "./componentes/rodape";
 import Box from '@mui/material/Box'
+import MenuSuperior from "./componentes/menusuperior";
 //import Formulario from "./frontend/componentes/formulario";                  
 function App() {
   return (
     <>
       <Home/>
       |<Rodape/>
-    </>
+      <MenuSuperior/>
+       </>
   );
 }
 
