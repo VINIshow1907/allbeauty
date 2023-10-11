785576ee41bbb78ffc89123d72ac0ec20e1d3a7f
Vinicius Silva <viniciuslucio792@gmail.com>
2023-10-11 13:27:19 -0300

    erro ao fazer a requisicao

--- backend/routes.ts
+++ backend/routes.ts
@@ -5,5 +5,8 @@ const router = Router();
 router.get('/teste', (req: Request, res:Response) => {
     return res.json({nome: 'vinicius'});
 })
+router.get('/teste', (req:Request, res:Response) => {
+    throw new Error('Erro ao fazer requisição');
+})
 
 export{router};
