e0082787467324ee64202a32f62b1aa2a67d3bf0
Vinicius Silva <viniciuslucio792@gmail.com>
2023-10-15 22:30:27 -0300

    importei para a pagina de cadastro

--- frontend/src/paginas/homeprofissional.tsx
+++ frontend/src/paginas/homeprofissional.tsx
@@ -3,14 +3,15 @@ import Rotas from "../rotas";
 import { Route, useNavigate, Navigate } from "react-router-dom";
 import ProMenuSuperior from "../componentes/promenusuperior";
 import Rodape from "../componentes/rodape";
-
+import Cadastro from "../paginas/cadastro";
 export default function EditarProfissional() {
     const navigate = useNavigate();
 
   return (
     <>
       <ProMenuSuperior/>
-      <h1>Inicial Profissional</h1>  
+      <h1>Inicial Profissional</h1> 
+      <Cadastro/>
  <Rodape/>
     </>
  );
