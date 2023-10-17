099a26a13d3cd3ae117fcbe468e43092aad0a6fd
Vinicius Silva <viniciuslucio792@gmail.com>
2023-10-17 15:36:41 -0300

    tirei as importacoes que não estou usando

--- frontend/src/paginas/home.tsx
+++ frontend/src/paginas/home.tsx
@@ -1,15 +1,7 @@
 import * as React from "react";
-import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import SwipeableViews from "react-swipeable-views";
 import { autoPlay } from "react-swipeable-views-utils";
-import primeiraimagem from "../img/primeiraimagem.png";
-import imgcarrossel4 from "../img/manicure1.jpg";
 import Rodape from "../componentes/rodape";
-import introducao from "../img/introducao.png"; 
 import { Route, useNavigate, Navigate } from "react-router-dom";
 import Menusuperior from "../componentes/menusuperior";
 import axios from "axios";
@@ -17,32 +9,51 @@ import { Grid } from "@mui/material";
 import Carrossel from "../componentes/carrosselhome";
 import "../estilos/fonte.css"; // Importe o arquivo CSS de estilos
 
-
 export default function Home() {
   const navigate = useNavigate();
 
   const paginaCadastro = () => {
     navigate("/Cadastro");
   };
-    
-    return (
-    <Box sx={{ maxWidth: 10000, flexGrow: 100,minHeight:250 ,textAlign: "center" }}>
+
+  return (
+    <Box
+      sx={{
+        maxWidth: 10000,
+        flexGrow: 100,
+        minHeight: 250,
+        textAlign: "center",
+      }}
+    >
       <Menusuperior />
-      <Carrossel/>    
-      
-      <Grid sx={{
-        marginLeft: 25,
-        marginRight: 25,
-        marginTop: 5,
-        marginBottom: 5,
-        color: "rgba(227,0,39,1)",
-        fontFamily: "monospace",
-      }}>
-        <h1>Agora, você cliente, poderá escolher o profissional que mais o agrada com base na cidade em que você está e no serviço que você precisa.</h1>
-        <h3 className="">Selecione o seu estado, a sua cidade e o serviço que procura para 
-        encontrar os profissionais que melhor atendem às suas necessidades.</h3>
+      <Carrossel />
+
+      <Grid
+        container
+        xs={12}
+        justifyContent="center" // centralize o conteudo horizontalmente
+        alignItems="center" // centraliza o conteudo verticalmente
+        sx={{
+          marginTop: 5,
+          marginBottom: 5,
+          color: "rgba(227,0,39,1)",
+          fontFamily: "monospace",
+          fontSize: "2em", //Tamanho da fonte padrão
+          textAlign: "center",
+          "@media (max-width: 768px)": {
+            fontSize: "1.5em", // Tamanho de fonte para dispositivos menores
+          },
+        }}
+      >
+        <Grid item xs={12}>
+          <h2> Escolha o Profissional que o mais agrada</h2>
+          <p>
+            Selecione o seu estado, a sua cidade e o serviço que procura para
+            encontrar os profissionais que melhor atendem às suas necessidades.
+          </p>
+        </Grid>
       </Grid>
-   <Rodape />
+      <Rodape />
     </Box>
   );
-              }
+}
