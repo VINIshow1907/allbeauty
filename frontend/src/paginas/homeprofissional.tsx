5f05f899b269a246a169f363760ce8b2140c1cb0
Vinicius Silva <viniciuslucio792@gmail.com>
2023-10-17 15:33:26 -0300

    troquei o componente para formularioeditarpro

--- frontend/src/paginas/homeprofissional.tsx
+++ frontend/src/paginas/homeprofissional.tsx
@@ -4,14 +4,17 @@ import { Route, useNavigate, Navigate } from "react-router-dom";
 import ProMenuSuperior from "../componentes/promenusuperior";
 import Rodape from "../componentes/rodape";
 import Cadastro from "../paginas/cadastro";
+import FormularioEditarPro from "../componentes/formularioeditarpro"
+
 export default function EditarProfissional() {
     const navigate = useNavigate();
 
   return (
     <>
       <ProMenuSuperior/>
-      <h1>Inicial Profissional</h1> 
-      <Cadastro/>
+      <h1>Edição Profissional</h1> 
+      <FormularioEditarPro/>
+
  <Rodape/>
     </>
  );
