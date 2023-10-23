c62cd733f7adace5be1e317a9260f71be2352a9c
Vinicius Silva <viniciuslucio792@gmail.com>
2023-10-23 20:32:45 -0300

    rotas certas

--- backend/routes/router.ts
+++ backend/routes/router.ts
@@ -1,14 +1,16 @@
 import {Router} from "express";
-
-import { CreateUserController } from "../src/controllers/user/CreateUserController";
-import { AuthUserController } from "../src/controllers/AuthUserController";
+// import { CreateUserController } from "../src/controllers/user/CreateUserController.js";
+// import { AuthUserController } from "../src/controllers/user/AuthUserController.js";
+//import { isAuthenticated } from "../src/middleware/isAuthenticated";
+import { DetailUserController } from "../src/controllers/user/DetailUserController.js";
+import { cadastroProfissional } from "../src/controllers/profissionalController.js";
 
 const router = Router();
 
 // ROTAS PARA USER//
-router.post('/user', new CreateUserController().handle)
-
-router.post('/session', new AuthUserController().handle)
-
+// router.post('/user', new CreateUserController().handle)
+// router.post('/session', new AuthUserController().handle)
+router.post('/cadastroprofissional', cadastroProfissional)
+//router.get('/userinfo', isAuthenticated, new DetailUserController().handle)
 
 export{router};
