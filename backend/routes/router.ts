744a8af0fbcf8c70e9c9af2de80863c6cd87a8e4
Vinicius Silva <viniciuslucio792@gmail.com>
2023-10-30 07:44:22 -0300

    adicoinei uma nova rota da pasta servicocontroller, puxando o pesquisaservico

--- backend/routes/router.ts
+++ backend/routes/router.ts
@@ -3,17 +3,16 @@ import {Router} from "express";
 // import { AuthUserController } from "../src/controllers/user/AuthUserController.js";
 //import { isAuthenticated } from "../src/middleware/isAuthenticated";
 //import { DetailUserController } from "../src/controllers/user/DetailUserController.js";
-import { cadastroProfissional } from "../src/controllers/profissionalController.js";
+import { cadastroProfissional,  } from "../src/controllers/profissionalController.js";
+import { pesquisaServico } from "../src/controllers/servicoController.js";
 
 const router = Router();
 
-// ROTAS PARA USER//
-// router.post('/user', new CreateUserController().handle)
-// router.post('/session', new AuthUserController().handle)
 router.post('/cadastroprofissional', cadastroProfissional)
+router.get('/pesquisaservico', pesquisaServico)
+router.get('')
 router.get('/teste', () => {
     console.log("Servidor recebeu req GET")
 })
-//router.get('/userinfo', isAuthenticated, new DetailUserController().handle)
 
 export{router};
