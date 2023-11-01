afc770d31a56073a00657bd791ffa063cd25415e
Vinicius Silva <viniciuslucio792@gmail.com>
2023-10-31 23:02:25 -0300

    adicionou a rota visualizar profissional

--- backend/routes/router.ts
+++ backend/routes/router.ts
@@ -3,13 +3,14 @@ import {Router} from "express";
 // import { AuthUserController } from "../src/controllers/user/AuthUserController.js";
 //import { isAuthenticated } from "../src/middleware/isAuthenticated";
 //import { DetailUserController } from "../src/controllers/user/DetailUserController.js";
-import { cadastroProfissional,  } from "../src/controllers/profissionalController.js";
+import { cadastroProfissional, visualizarprofissional,  } from "../src/controllers/profissionalController.js";
 import { pesquisaServico } from "../src/controllers/servicoController.js";
 
 const router = Router();
 
 router.post('/cadastroprofissional', cadastroProfissional)
 router.get('/pesquisaservico', pesquisaServico)
+router.get('/visualizarprofissional', visualizarprofissional)
 router.get('')
 router.get('/teste', () => {
     console.log("Servidor recebeu req GET")
