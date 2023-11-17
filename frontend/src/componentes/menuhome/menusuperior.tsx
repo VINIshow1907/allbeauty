867a9ee68a6314cb81a99907871b820402c27dc8
Vinicius Silva <viniciuslucio792@gmail.com>
2023-11-17 16:37:11 -0300

    tirei o menu icone

--- frontend/src/componentes/menuhome/menusuperior.tsx
+++ frontend/src/componentes/menuhome/menusuperior.tsx
@@ -35,7 +35,7 @@ export default function MenuSuperior() {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             AllBeauty
