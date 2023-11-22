4e62c9c2a8dd5bdb6d4664998a07e3d908087302
Vinicius Silva <viniciuslucio792@gmail.com>
2023-11-21 22:47:42 -0300

    rota que associa o profissoinal e o servico

--- backend/routes/router.ts
+++ backend/routes/router.ts
@@ -3,16 +3,15 @@ import {Router} from "express";
 // import { AuthUserController } from "../src/controllers/user/AuthUserController.js";
 //import { isAuthenticated } from "../src/middleware/isAuthenticated";
 //import { DetailUserController } from "../src/controllers/user/DetailUserController.js";
-import { cadastroProfissional, visualizarprofissional,  } from "../src/controllers/profissionalController.js";
+import { associarProfissionalServico, cadastroProfissional, visualizarprofissional,  } from "../src/controllers/profissionalController.js";
 import { pesquisaServico } from "../src/controllers/servicoController.js";
 
 const router = Router();
 
 router.post('/cadastroprofissional', cadastroProfissional)
 router.get('/pesquisaservico', pesquisaServico)
-
-router.get('/visualizarprofissional', visualizarprofissional)
-router.get('')
+router.post('/associarprofissionalservico', associarProfissionalServico)
+router.get('/visualizarprofissional/:id', visualizarprofissional)
 router.get('/teste', () => {
     console.log("Servidor recebeu req GET")
 })
