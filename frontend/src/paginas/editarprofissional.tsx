2ffb4171eea023e2cd38aa8158a2ca50a8a8dabc
Vinicius Silva <viniciuslucio792@gmail.com>
2023-11-22 16:47:10 -0300

    mudei a pagina para colocar o menueditarpro

--- frontend/src/paginas/editarprofissional.tsx
+++ frontend/src/paginas/editarprofissional.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import Rotas from "../rotas"
 import FormularioEditarPro from "../componentes/editarprofissional/formularioeditarpro"
 import ProMenuSuperior from "../componentes/menusuperiorpro/promenusuperior"
+import MenuEditarPro from"../componentes/menueditarpro"
 import { Box, Typography } from "@mui/material"
 import Rodape from "../componentes/rodape/rodape"
 
@@ -10,7 +11,7 @@ export default function EditarProfissional() {
     return (
 
         <>
-       <ProMenuSuperior />
+       <MenuEditarPro />
        <FormularioEditarPro />
        <Rodape />
        
