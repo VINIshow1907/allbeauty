c574708f862ffa2bb9d3b65cef80fdc3f6ea686c
Vinicius Silva <viniciuslucio792@gmail.com>
2023-11-22 23:17:34 -0300

    deletar, buscar e editar o profissional adicionei as rotas

--- backend/routes/router.ts
+++ backend/routes/router.ts
@@ -3,7 +3,7 @@ import {Router} from "express";
 // import { AuthUserController } from "../src/controllers/user/AuthUserController.js";
 //import { isAuthenticated } from "../src/middleware/isAuthenticated";
 //import { DetailUserController } from "../src/controllers/user/DetailUserController.js";
-import { associarProfissionalServico, cadastroProfissional, visualizarprofissional,  } from "../src/controllers/profissionalController.js";
+import { associarProfissionalServico, cadastroProfissional, editarProfissional, visualizarprofissional, deletarprofissional, buscaprofissional } from "../src/controllers/profissionalController.js";
 import { pesquisaServico } from "../src/controllers/servicoController.js";
 
 const router = Router();
@@ -12,6 +12,9 @@ router.post('/cadastroprofissional', cadastroProfissional)
 router.get('/pesquisaservico', pesquisaServico)
 router.post('/associarprofissionalservico', associarProfissionalServico)
 router.get('/visualizarprofissional/:id', visualizarprofissional)
+router.delete('/deletarprofissional/:id', deletarprofissional)
+router.get('/buscaprofissional', buscaprofissional)
+router.put('/editarprofissional/:id', editarProfissional)
 router.get('/teste', () => {
     console.log("Servidor recebeu req GET")
 })
